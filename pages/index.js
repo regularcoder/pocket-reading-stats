e4629a8ce14f62e98009cd9437ab65388b327ef1
Nikhil <osregularcoder@gmail.com>
2022-10-02 19:03:36 +0530

    Nikhil: Basic retrieval and stats - for just past week

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,55 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 import Cookies from 'cookies'
+import { useEffect, useState } from 'react'
 
-export default function Home() {
+export default function Home({ list }) {
+  const [wordsInWeek, setWordsInWeek] = useState(0);
+  
+  useEffect(() => {
+    let wordsInPeriod = 0;
+
+    Object.entries(list).forEach(([, value]) => {
+      wordsInPeriod += Number(value.word_count);
+    });
+
+    setWordsInWeek(wordsInPeriod);
+  });
+
 
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          
-        <Link href="/start-auth">
-          <a>Authorise</a>
-        </Link>
-        </p>
-      </main>
-      <Footer />
+    <div>
+      <h2>Articles read in past 7 days</h2>
+      Total words: {wordsInWeek}<br/><br/>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Words</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(list).map(([key, value]) => {
+            return <tr key={value.item_id}>
+              <td>{value.resolved_title || value.given_title}</td>
+              <td>{value.word_count}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
 
+function lastWeek() {
+  var today = new Date();
+  var nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+  return Math.floor(nextweek.getTime() / 1000);
+}
+
 export async function getServerSideProps({ req, res }) {
   const cookies = new Cookies(req, res);
-  const code = cookies.get('access-token');
+  const access_token = cookies.get('access-token');
 
-  if (!code) {
+  if (!access_token) {
     return {
       redirect: {
         destination: '/start-auth',
@@ -40,7 +58,24 @@ export async function getServerSideProps({ req, res }) {
     }
   }
 
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      consumer_key: process.env.POCKET_CONSUMER_KEY,
+      access_token,
+      state: 'archive',
+      since: lastWeek()
+    })
+  };
+
+  const oauth_response = await fetch(`https://getpocket.com/v3/get`, options);
+
+  const json_response = await oauth_response.json();
+
   return {
-    props: {},
+    props: { list: json_response.list },
   }
 }
